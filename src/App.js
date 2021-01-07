d77ac46218bf56eece0ac10740b64adafc159eb2
deveshrawat <devsrawt@gmail.com>
2021-01-07 23:48:25 +0530

    Improve App.js

--- src/App.js
+++ src/App.js
@@ -5,25 +5,34 @@ import "./App.css";
 const App = () => {
   const [accounts, setAccounts] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [web3, setWeb3] = useState({});
   // const [contract, setContract] = useState(null);
 
   useEffect(() => {
     setupWeb3();
+    return () => window.removeEventListener("load");
   }, []);
 
   const setupWeb3 = async () => {
     try {
       const web3 = await getWeb3();
       const accounts = await web3.eth.getAccounts();
-      /*    const instance = await new web3.eth.Contract(
-        WeenusTokenABI,
-        contractAddress
+
+      // Get the contract instance.
+      // const networkId = await web3.eth.net.getId();
+      // const deployedNetwork = SimpleStorageContract.networks[networkId];
+      /*    const instance = new web3.eth.Contract(
+        SimpleStorageContract.abi,
+        deployedNetwork && deployedNetwork.address
       ); */
+
       window.web3 = web3;
       setAccounts(accounts);
       // setContract(instance);
       web3.eth.defaultAccount = accounts[0];
       setLoading(false);
+      setWeb3(web3);
+      // setContract(instance);
     } catch (error) {
       alert(
         `Failed to load web3, accounts, or contract. Check console for details.`
